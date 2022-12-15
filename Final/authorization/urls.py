91404b0f629f0eb589c95c817824473da0c8c3ea
Albina210109038 <210109038@stu.sdu.edu.kz>
2022-12-16 00:53:32 +0600

    Authorization is DONE

--- Final/authorization/urls.py
+++ Final/authorization/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    path('authorization/', views.SignIn, name='sign_in')
+    path('sign-in', views.SignIn, name='sign_in'),
+    path('sign-up', views.SignUp, name='sign_up'),
+    path('sign-out', views.SignOut, name='sign_out'),
 ]
--- Final/authorization/views.py
+++ Final/authorization/views.py
@@ -1,5 +1,61 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 
 def SignIn(request):
-    pass
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('sign_in')
+
+    return render(request, 'authorization/sign_in.html')
+
+
+def SignUp(request):
+
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('sign_up')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('sign_up')
+            else:
+                new_user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=confirm_password
+                )
+                new_user.save()
+                messages.success(request, 'User successfully created')
+                return redirect('sign_in')
+        else:
+            messages.info(request, 'We declared mistakes! Repeat again, please!')
+            return redirect('sign_up')
+
+    return render(request, 'authorization/sign_up.html')
+
+
+def SignOut(request):
+    auth.logout(request)
+    return redirect('/')
