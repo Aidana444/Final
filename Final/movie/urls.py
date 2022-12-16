ffc3557fc703a62e7cda6ca1434937b2ad1b9b58
Aidana444 <210103345@stu.sdu.edu.kz>
2022-12-17 01:59:33 +0600

    views and urls is DONE!

--- Final/movie/urls.py
+++ Final/movie/urls.py
@@ -4,4 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='home'),
+    path('oscar', views.OscarPage, name='oscar'),
+    path('movies', views.MoviesPage, name='movies'),
+    path('series', views.SeriesPage, name='series'),
+    path('cartoons', views.CartoonsPage, name='cartoons'),
+    path('cybersport', views.CybersportPage, name='cybersport'),
+    path('top5', views.TopPage, name='top5'),
+    path('subscription', views.SubscriptionPage, name='subscription'),
+
+    path('detail_product', views.DetailProductPage, name='detail_product'),
+
+    path('profile', views.ProfilePage, name='profile')
 ]
--- Final/movie/views.py
+++ Final/movie/views.py
@@ -3,3 +3,39 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'movie/home.html')
+
+
+def OscarPage(request):
+    return render(request, 'movie/oscar.html')
+
+
+def MoviesPage(request):
+    return render(request, 'movie/movies.html')
+
+
+def SeriesPage(request):
+    return render(request, 'movie/series.html')
+
+
+def CartoonsPage(request):
+    return render(request, 'movie/cartoons.html')
+
+
+def TopPage(request):
+    return render(request, 'movie/top-5.html')
+
+
+def SubscriptionPage(request):
+    return render(request, 'movie/aboutsubscription.html')
+
+
+def CybersportPage(request):
+    return render(request, 'movie/esports.html')
+
+
+def DetailProductPage(request):
+    return render(request, 'movie/watch-product.html')
+
+
+def ProfilePage(request):
+    return render(request, 'movie/profile.html')
